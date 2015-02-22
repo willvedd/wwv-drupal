b7f94d6bf5a102dd9eb7e3dce0d51a9f01243cde
Will V <willvedd@gmail.com>
2015-02-22 01:02:25 -0500

    Moved GA into head

--- sites/all/themes/wwv/templates/html.tpl.php
+++ sites/all/themes/wwv/templates/html.tpl.php
@@ -34,18 +34,18 @@
   <meta http-equiv="cleartype" content="on">
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-60014001-1', 'auto');
+    ga('send', 'pageview');
+
+  </script>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
   <?php print $page; ?>
 </body>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-60014001-1', 'auto');
-  ga('send', 'pageview');
-
-</script>
 </html>
