ec79c60e9b4c7b5fdb1645cd4dfde227c9813f90
Will V <willvedd@gmail.com>
2015-02-21 23:05:58 -0500

    Styling things

--- sites/all/themes/wwv/templates/includes/header.tpl.php
+++ sites/all/themes/wwv/templates/includes/header.tpl.php
@@ -1,9 +1,11 @@
 <header role="banner">
-  <h1>
-  	<a class="header" href="<?php echo $GLOBALS['base_url'];?>">
-  		<span id="name">Will</span> Vedder
-  	</a>
-  	<div class="underline"></div>
-  </h1>
+  	<div class="h1contain">
+	  <h1>
+	  	<a class="header" href="<?php echo $GLOBALS['base_url'];?>">
+	  		<span id="name">Will</span> Vedder
+	  		<div class="underline"></div>
+	  	</a>
+	  </h1>
+	</div>
 </header>
 <img alt="decorative chevron" class="header_overlay" src="<?php echo $GLOBALS['base_url'].'/'.path_to_theme().'/images/header_chevron2.svg';?>"/>
